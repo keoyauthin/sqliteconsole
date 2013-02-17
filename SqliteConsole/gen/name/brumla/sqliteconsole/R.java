86b4b88fe389c4de0f04ed3942878dd8ca5b491f
Brumla <Brumla@homepc>
2013-02-17 17:58:59 +0100

    Setting skeleton Database SQL commands execution Translation CZ

--- SqliteConsole/gen/name/brumla/sqliteconsole/R.java
+++ SqliteConsole/gen/name/brumla/sqliteconsole/R.java
@@ -17,33 +17,47 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_reset=0x7f080005;
-        public static final int action_sample_db=0x7f080004;
-        public static final int action_settings=0x7f080003;
-        public static final int btExecute=0x7f080002;
-        public static final int etQuery=0x7f080000;
-        public static final int etResult=0x7f080001;
+        public static final int action_about=0x7f0a0005;
+        public static final int action_reset=0x7f0a000b;
+        public static final int action_sample_db=0x7f0a0004;
+        public static final int action_settings=0x7f0a0003;
+        public static final int btExecute=0x7f0a0002;
+        public static final int etQuery=0x7f0a0000;
+        public static final int etResult=0x7f0a0001;
+        public static final int sqlNext=0x7f0a000a;
+        public static final int sqlNotNull=0x7f0a0008;
+        public static final int sqlPK=0x7f0a0006;
+        public static final int sqlPrev=0x7f0a0009;
+        public static final int sqlVarchar=0x7f0a0007;
     }
     public static final class layout {
         public static final int activity_console=0x7f030000;
     }
     public static final class menu {
-        public static final int console=0x7f070000;
+        public static final int console=0x7f090000;
+    }
+    public static final class raw {
+        public static final int sample=0x7f050000;
     }
     public static final class string {
-        public static final int action_console_btexecute=0x7f050003;
-        public static final int action_console_btreset=0x7f050004;
-        public static final int action_console_menu_sample=0x7f050005;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int about_application=0x7f070008;
+        public static final int about_text=0x7f070009;
+        public static final int action_about=0x7f070006;
+        public static final int action_console_btexecute=0x7f070003;
+        public static final int action_console_btreset=0x7f070004;
+        public static final int action_console_menu_sample=0x7f070005;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070002;
+        public static final int msg_database_created=0x7f070007;
+        public static final int title_activity_settings=0x7f07000a;
     }
     public static final class style {
         /** 
@@ -67,10 +81,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int pref_general=0x7f040000;
+        public static final int pref_headers=0x7f040001;
     }
 }
--- SqliteConsole/src/name/brumla/sqliteconsole/ConsoleActivity.java
+++ SqliteConsole/src/name/brumla/sqliteconsole/ConsoleActivity.java
@@ -1,15 +1,106 @@
 package name.brumla.sqliteconsole;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import name.brumla.sqliteconsole.db.BaseDbHelper;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ConsoleActivity extends Activity {
+
+	private BaseDbHelper baseDbHelper;
+	private SQLiteDatabase writableDatabase;
+	private Button btExecute;
+	private EditText teQuery;
+	private EditText teResult;
+
+	private List<String> sqlQueries = new ArrayList<String>();
+	int pos = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_console);
+
+		baseDbHelper = new BaseDbHelper(this, null, null, 1);
+		writableDatabase = baseDbHelper.getWritableDatabase();
+
+		Log.d(ConsoleActivity.class.getName(), "Writeble database at "
+				+ writableDatabase);
+
+		btExecute = (Button) findViewById(R.id.btExecute);
+		teQuery = (EditText) findViewById(R.id.etQuery);
+		teResult = (EditText) findViewById(R.id.etResult);
+
+		initializeActions();
+	}
+
+	private void initializeActions() {
+		btExecute.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				teResult.setText("");
+				String sql = teQuery.getText().toString();
+				Log.d(this.getClass().getName(), "Query: " + sql);
+
+				try {
+					Cursor cr = ConsoleActivity.this.writableDatabase.rawQuery(
+							sql, null);
+					Log.d(this.getClass().getName(), "Result is " + cr);
+
+					if (cr.getCount() == 0) {
+						teResult.getText().append("OK\n");
+						teResult.getText().append("-- no result --");
+					} else {
+						teResult.getText().append("OK\n");
+
+						for (int i = 0; i < cr.getColumnCount(); i++) {
+							teResult.getText().append(
+									cr.getColumnName(i) + " |");
+						}
+						teResult.getText().append("\n");
+						cr.moveToFirst();
+
+						do {
+							for (int i = 0; i < cr.getColumnCount(); i++) {
+								teResult.getText().append(
+										cr.getString(i) + " |");
+							}
+							teResult.getText().append("\n");
+						} while (cr.moveToNext());
+					}
+
+					if (ConsoleActivity.this.writableDatabase.inTransaction()) {
+						Log.d(this.getClass().getName(), "COMMIT");
+						ConsoleActivity.this.writableDatabase
+								.setTransactionSuccessful();
+						ConsoleActivity.this.writableDatabase.endTransaction();
+					}
+				} catch (SQLiteException e) {
+					if (ConsoleActivity.this.writableDatabase.inTransaction()) {
+						Log.d(this.getClass().getName(), "ROLLBACK");
+						ConsoleActivity.this.writableDatabase.endTransaction();
+					}
+					teResult.getText().append("ERROR\n");
+					teResult.getText().append("Exception :" + e.getMessage());
+				}
+			}
+		});
 	}
 
 	@Override
@@ -19,4 +110,76 @@ public class ConsoleActivity extends Activity {
 		return true;
 	}
 
+	public SQLiteDatabase getWritableDatabase() {
+		return writableDatabase;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d(this.getClass().getName(), "Menu item id = " + item.getItemId());
+
+		String sql = "";
+		switch (item.getItemId()) {
+		case R.id.sqlPK:
+			sql = teQuery.getText().toString();
+			teQuery.setText(sql + " INTEGER PRIMARY KEY AUTOINCREMENT ");
+			break;
+		case R.id.sqlNotNull:
+			sql = teQuery.getText().toString();
+			teQuery.setText(sql + " NOT NULL ");
+			break;
+		case R.id.sqlVarchar:
+			sql = teQuery.getText().toString();
+			teQuery.setText(sql + " VARCHAR( ");
+			break;
+		case R.id.action_reset:
+			sqlQueries.add(teQuery.getText().toString());
+
+			if (sqlQueries.size() > 1000) {
+				sqlQueries.remove(0);
+			}
+			teQuery.setText("");
+			pos = sqlQueries.size() - 1;
+			Log.d(this.getClass().getName(), "Position updated: " + pos);
+			break;
+		case R.id.sqlNext:
+			if (pos < sqlQueries.size() - 1) {
+				pos++;
+				teQuery.setText(sqlQueries.get(pos));
+				Log.d(this.getClass().getName(), "position updated");
+			} else {
+				Log.d(this.getClass().getName(), "position not updated");
+			}
+			break;
+		case R.id.sqlPrev:
+			if (pos > 0) {
+				pos--;
+				teQuery.setText(sqlQueries.get(pos));
+				Log.d(this.getClass().getName(), "position updated");
+			} else {
+				Log.d(this.getClass().getName(), "position not updated");
+			}
+			break;
+		case R.id.action_sample_db:
+			baseDbHelper = new BaseDbHelper(this, null, null, 1);
+			writableDatabase = baseDbHelper.getWritableDatabase();
+			teQuery.setText("");
+			teResult.setText(R.string.msg_database_created);
+			sqlQueries.clear();
+			break;
+		case R.id.action_about:
+			AlertDialog.Builder bld = new Builder(this);
+			bld.setTitle(R.string.about_application);
+			bld.setMessage(R.string.about_text);
+			bld.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+				}});
+			bld.create().show();
+		}
+
+		return true;
+	}
 }
